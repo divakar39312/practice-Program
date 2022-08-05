5095111485072e224b398e5fac9626c9529d0004
Divakar <divakar39312@gmail.com>
2022-08-05 21:10:51 +0530

    [Divakar]:count No of digit added

---
+++ src/com/Bridgelabz/NoofDigitInInteger.java
@@ -0,0 +1,22 @@
+package com.Bridgelabz;
+
+import java.util.Scanner;
+
+public class NoofDigitInInteger {
+
+	public static void main(String[] args) {
+
+		Scanner sc=new Scanner(System.in);
+	    int num	=sc.nextInt();
+	    int count = 0; //num = 0003452;
+       
+	    while (num != 0) {
+	      // num = num/10
+	      num /= 10;
+	      ++count;
+	    }
+
+	    System.out.println("Number of digits: " + count);
+	  }
+
+}
--- src/com/Bridgelabz/ReverseString.java
+++ src/com/Bridgelabz/ReverseString.java
@@ -1,9 +1,13 @@
 package com.Bridgelabz;
 
+import java.util.Scanner;
+
 public class ReverseString {
 
 	public static void main(String[] args) {
-		String str ="Hello";
+		//String str ="Hello";
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
 		char ch[]=str.toCharArray();  
 	    String rev="";  
 	    for(int i=ch.length-1;i>=0;i--){  
