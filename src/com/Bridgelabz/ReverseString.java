b16b1225694e66aa26f5bf59c986d1045dfd50dd
Divakar <divakar39312@gmail.com>
2022-08-05 09:44:46 +0530

    [Divakar]:Reverse string added

---
+++ src/com/Bridgelabz/ReverseString.java
@@ -0,0 +1,14 @@
+package com.Bridgelabz;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		String str ="Hello";
+		char ch[]=str.toCharArray();  
+	    String rev="";  
+	    for(int i=ch.length-1;i>=0;i--){  
+	        rev+=ch[i];  
+	     }
+	    System.out.println(rev);
+	}
+}
