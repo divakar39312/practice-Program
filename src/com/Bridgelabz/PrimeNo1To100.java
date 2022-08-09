90cc929ba72966575fb3e11a4bdfc066d945ca9d
Divakar <divakar39312@gmail.com>
2022-08-09 09:58:17 +0530

    [Divakar]:file added

---
+++ src/com/Bridgelabz/PrimeNo1To100.java
@@ -0,0 +1,26 @@
+package com.Bridgelabz;
+
+public class PrimeNo1To100 {
+
+	public static void main (String[] args)
+	   {		
+	       int temp=0;
+	       for(int num=1;num<=100;num++)
+	       {
+	    	   for(int i=2;i<=num-1;i++)
+	    	   {
+	    		   if(num%i==0)
+	    		   {
+	    			   temp=temp+1;
+	    		   }
+	    	   }
+	    	   if(temp==0)
+	    	   {
+	    		   System.out.println(num);
+	    	   }else {
+	    		   temp=0;
+	    	   }
+	       }
+
+ }
+}
